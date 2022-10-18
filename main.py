983266402c4e3895abad5d9f1ffdfc3840059d99
JSanchez78657 <joel.sanchez78657@gmail.com>
2022-10-18 13:36:46 -0700

    Simplified the math. Was totally unnecessary. Also fixed the page ranging to dramatically improve run-time.

--- main.py
+++ main.py
@@ -6,24 +6,35 @@ from PIL import Image
 from PIL import ImageFilter
 
 # PDF Qualities
-starting_page = 1
-ending_page = 6
+# Full Cards 1 - 94, 109 - 110
+# Half Cards 95 - 108
+starting_page = 95
+ending_page = 108
 
 # Page Qualities
 rows_per_page = 3
 columns_per_page = 3
 cards_per_page = rows_per_page * columns_per_page
 
-# Card qualities
-origin = 79, 29
-width = 503
-height = 703
-x_offset = 17
-y_offset = 17
+# Card qualities (default resolution: 1700, 2200)
+# Full Cards
+# origin = 79, 29
+# width = 503
+# height = 703
+# x_offset = 520
+# y_offset = 720
+# Mini Cards
+origin = 168, 131
+width = 325
+height = 499
+x_offset = 519
+y_offset = 720
 
 # Store Pdf with convert_from_path function
-images = convert_from_path('BattleCON V4 Errata.pdf')
+images = convert_from_path('PnP (Major and Minor).pdf', first_page=starting_page, last_page=ending_page)
 cards = []
+fronts = []
+backs = []
 
 
 def is_blank(img):
@@ -37,7 +48,9 @@ if ending_page > images.__sizeof__():
     print("PDF is not long enough for the requested range.")
     sys.exit()
 
-for i in range(starting_page - 1, ending_page):
+card_count = 0
+
+for i in range(ending_page - starting_page + 1):
     # Save pages as images in the pdf
     image = images[i]
     selection = [origin[0], origin[1]]
@@ -46,26 +59,30 @@ for i in range(starting_page - 1, ending_page):
     for x in range(3):
         # Y is for columns of cards.
         for y in range(3):
-            # Mathematical bullshit to get correct indexes.
-            # Each page has a 3x3 grid of cards with 9 cards total.
-            # x and y are used to find the position of the card on a given page, each index incrementing by 2.
-            # i is used to determine if the card is a front or back (even page or odd page) and to offset the index appropriately.
-            # If it is a front (even), simply offset the index to reflect the card count.
-            # If it is a back (odd), set the offset to be one more than the matching card on the previous page.
-            index = (x * columns_per_page * 2) + (y * 2) + (i * cards_per_page if i % 2 == 0 else (i - 1) * cards_per_page + 1)
             card = image.crop((selection[0], selection[1], selection[0] + width, selection[1] + height))
-            cards.insert(index, card)
-            selection[0] = selection[0] + width + x_offset
+            if not is_blank(card.load()):
+                # card_count += 1
+                # card_name = "cards/"
+                # if card_count < 100:
+                #     card_name += "0"
+                # if card_count < 10:
+                #     card_name += "0"
+                # card_name += str(card_count)
+                if i % 2 == 0:
+                    # card_name += "_front.jpg"
+                    fronts.append(card)
+                    # card.save(card_name)
+                else:
+                    # card_name += "_back.jpg"
+                    backs.append(card)
+                    # card.save(card_name)
+            selection[0] = selection[0] + x_offset
         selection[0] = origin[0]
-        selection[1] = selection[1] + height + y_offset
+        selection[1] = selection[1] + y_offset
 
-blanks = []
-for card in cards:
-    if is_blank(card.load()):
-        blanks.append(card)
-
-for card in blanks.__reversed__():
-    cards.remove(card)
+for i in range(len(fronts)):
+    cards.append(fronts[i])
+    cards.append(backs[i])
 
 if cards.__sizeof__() > 1:
     cards[0].save('ReformattedPDF.pdf', save_all=True, append_images=cards[1:])
